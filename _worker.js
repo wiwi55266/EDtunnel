41fc7e8d6dad802d9aedaa911a6043c76dcecba8
wiwi55266 <159012005+wiwi55266@users.noreply.github.com>
2024-10-13 00:45:05 +0700

    miyt

--- _worker.js
+++ _worker.js
@@ -3,12 +3,11 @@ const listProxy = [
     { path: '/akamai', proxy: '172.232.238.169' },
     { path: '/kr', proxy: '52.141.25.42'},
     { path: '/us', proxy: '91.186.208.191'},
-    { path: '/gcp', proxy: '103.133.223.52' },
+    { path: '/gcp', proxy: '34.34.217.201' },
     { path: '/do', proxy: '188.166.255.195' },
     { path: '/do2', proxy: '143.198.213.197' },
     { path: '/incapsula', proxy: '45.60.186.91' },
     { path: '/ovh', proxy: '15.235.162.49' },
-    { path: '/rumahweb', proxy: '203.194.112.119' },
 ];
 let proxyIP;
 export default {
